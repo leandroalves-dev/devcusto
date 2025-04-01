c73faa14e1b4d2adcfabee8a328199289fafcb56
leandroeffgen8 <leandroeffgen8@gmail.com>
2025-04-01 19:45:12 -0300

    feat(page): Criando a pagina de contato

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,9 +1,83 @@
+import { useState } from "react"
+import Buttons from "../../components/Buttons"
 import Container from "../../components/Container"
+import Input from "../../components/Input"
 
 const Contact = () => {
+
+    const [formData, setFormData] = useState({
+        nome: '',
+        email: '',
+        cidade: '',
+        estado: '',
+        telefone: ''
+    })
+
+    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) =>{
+        setFormData({ ...formData, [e.target.name]: e.target.value})
+    }
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+        console.log('Dados do formulário ->', formData)
+    }
+
     return (
-        <Container>
-            <h1>Contact</h1>
+        <Container className="py-6">
+            <div className="mx-auto w-full max-w-3xl">
+                <h1 className="text-white text-2xl pb-3">Fale Conosco</h1>
+                <p className="text-zinc-500 leading-5 pb-4">
+                    Estamos aqui para ajudar! Se você tiver dúvidas, sugestões ou feedback sobre o DevCusto, entre em contato conosco.
+                    Queremos ouvir suas opiniões e melhorar cada vez mais a nossa plataforma.
+                </p>
+                <form onSubmit={handleSubmit} className="w-full">
+                    <Input
+                        type="text"
+                        label="Seu nome"
+                        name="nome"
+                        value={formData.nome}
+                        placeholder="Insira seu nome"
+                        onChange={handleChange}
+                    />
+                
+                    <Input
+                        type="email"
+                        label="Seu e-mail"
+                        name="email"
+                        value={formData.email}
+                        placeholder="Insira seu e-mail"
+                        onChange={handleChange}
+                    />
+                   
+                    <div className="flex flex-col sm:flex-row sm:gap-5 gap-0 w-full">
+                        <Input
+                            type="text"
+                            label="Sua cidade"
+                            name="cidade"
+                            value={formData.cidade}
+                            placeholder="Insira sua cidade"
+                            onChange={handleChange}
+                        />
+                        <Input
+                            type="text"
+                            label="Seu estado"
+                            name="estado"
+                            value={formData.estado}
+                            placeholder="Insira seu estado"
+                            onChange={handleChange}
+                        />
+                        <Input
+                            type="text"
+                            label="Seu telefone"
+                            name="telefone"
+                            value={formData.telefone}
+                            placeholder="(xx) xxxxx-xxxx"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <Buttons title="Enviar" />
+                </form>
+            </div>
         </Container>
     )
 }
--- src/pages/Projects/index.tsx
+++ src/pages/Projects/index.tsx
@@ -92,15 +92,15 @@ const ProjectList = () => {
                     <Message title="Desculpe, mais ainda não temos projetos cadastrados!" />
                 )}
 
-                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 text-white">
+                <ul className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4 text-white">
                    
                     {projects.map(project => (
-                        <li key={project.id} className="bg-neutral-900 mb-5 text-zinc-400">
-                            <h3 className="p-3 h-[60px] flex items-center text-left leading-4 bg-neutral-800 text-white capitalize">
+                        <li key={project.id} className="bg-[#0f0f0f] mb-5 text-zinc-400">
+                            <h3 className="p-3 h-[60px] flex items-center text-left leading-4 bg-[#101010] border-b border-[#141414] text-white capitalize">
                                 {project.name}
                             </h3>
                             <div className="p-3 flex flex-col gap-1">
-                                <p className="text-sm leading-4 my-3 border-b border-neutral-800 pb-4">{project.description}</p>
+                                <p className="text-sm leading-4 my-3 border-b border-[#141414] pb-4">{project.description}</p>
                                 <p className="text-sm flex justify-between items-center">
                                     <span>Orçamento:</span>
                                     <span className="text-[12px]">{formatCurrency(project.budget)}</span>
@@ -116,12 +116,12 @@ const ProjectList = () => {
                                 </p>
                                 <div className="flex justify-between items-center mt-2 gap-2">
                                     <button onClick={() => handleEdit(project)}
-                                        className="flex items-center gap-1 justify-center py-2 w-full bg-neutral-800 text-sm">
+                                        className="flex items-center gap-1 justify-center py-2 w-full bg-[#121212] cursor-pointer text-sm">
                                         <BsPencil className="text-green-500" />
                                         Editar
                                     </button>
                                     <button onClick={() => handleDelete(project.id)}
-                                        className="flex items-center gap-1 justify-center py-2 w-full bg-neutral-950 text-sm">
+                                        className="flex items-center gap-1 justify-center py-2 w-full bg-[#121212] cursor-pointer text-sm">
                                         <BsTrash className="text-red-500" />
                                         Excluir
                                     </button>
